8a7fab42cfe337e01a9b6b58c876bc707d86d9e7
Hugo Torres <hugo.torres1993@gmail.com>
2018-10-29 08:30:26 -0400

    Add command scaffolding

---
+++ cmd/client/cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "runlogq",
+	Short: "runlogq is a client for runlog",
+	Long:  `TODO: fill me out`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("hello client")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
